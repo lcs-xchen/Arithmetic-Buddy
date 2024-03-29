fc2b05bec8cc6cf3c882318d4247d1c0de4e3a34
Cindy Chen <xchen@lcs.on.ca>
2024-03-29 18:15:41 -0400

    Initial Commit

---
+++ Arithmetic Buddy/Arithmetic_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_BuddyApp.swift
+//  Arithmetic Buddy
+//
+//  Created by Cindy Chen on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
